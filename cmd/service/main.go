625083d2dc462dda80caaaaf05371852ae4f0a1b
aleksakokorewa <134208188+aleksakokorewa@users.noreply.github.com>
2024-07-08 18:09:20 +0300

    task1 created project's architecture + config

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/s21platform/friends-service/internal/config"
 )
 
 func main() {
 	//чтение конфига
-	cfg := config.MustLoad()
-	fmt.Printf("%+v\n", cfg)
+	_ = config.MustLoad()
 }
