f5aa8f1eb6d857d46b9cb8a7ce29a765458b02de
aleksakokorewa <134208188+aleksakokorewa@users.noreply.github.com>
2024-07-28 14:44:04 +0300

    added migrations

--- internal/repositore/db/repository.go
+++ internal/repositore/db/repository.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"github.com/s21platform/friends-service/internal/config"
 	"log"
