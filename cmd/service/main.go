679f889256872f7bd7cb449ddb2925a64209f7ad
Aleksey Popov <asepopov@avito.ru>
2024-07-28 22:28:07 +0300

    #17 fix logging for errors

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	//чтение конфига
 	cfg := config.MustLoad()
-	log.Printf("postgres host=%s port=%s user=%s dbname=%s", cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User, cfg.Postgres.Database)
-	fmt.Printf("postgres host=%s port=%s user=%s dbname=%s", cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User, cfg.Postgres.Database)
+	log.Printf("postgres log host=%s port=%s user=%s dbname=%s\n", cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User, cfg.Postgres.Database)
+	fmt.Printf("postgres fmt host=%s port=%s user=%s dbname=%s\n", cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User, cfg.Postgres.Database)
 	Data, err := db.New(cfg)
 	if err != nil {
 		log.Fatal(err)
