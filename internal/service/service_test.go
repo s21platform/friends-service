b4e64ce45457e48dd229476a460a6bfbe17965f0
aleksakokorewa <134208188+aleksakokorewa@users.noreply.github.com>
2024-08-07 23:42:17 +0300

    added tests for getWhoFollowsPeer

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -49,3 +49,43 @@ func TestServer_GetPeerFollow(t *testing.T) {
 
 	})
 }
+
+func TestServer_GetWhoFollowPeer(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDbRepo := NewMockDbRepo(ctrl)
+
+	t.Run("should_ok_with_UUID", func(t *testing.T) {
+		userUUID := uuid.Generate()
+		followersUUID := []string{
+			uuid.Generate().String(),
+			uuid.Generate().String(),
+		}
+		mockDbRepo.EXPECT().GetWhoFollowsPeer(userUUID.String()).Return(followersUUID, nil)
+
+		s := New(mockDbRepo)
+		res, err := s.GetWhoFollowPeer(ctx, &friends_proto.GetWhoFollowPeerIn{Uuid: userUUID.String()})
+		assert.NoError(t, err)
+		assert.Equal(t, res, &friends_proto.GetWhoFollowPeerOut{Subscribers: []*friends_proto.Peer{
+			&friends_proto.Peer{Uuid: followersUUID[0]},
+			&friends_proto.Peer{Uuid: followersUUID[1]},
+		}})
+
+	})
+
+	t.Run("should_repo_err", func(t *testing.T) {
+		userUUID := uuid.Generate()
+		repoErr := errors.New("test")
+
+		mockDbRepo.EXPECT().GetWhoFollowsPeer(userUUID.String()).Return(nil, repoErr)
+
+		s := New(mockDbRepo)
+		_, err := s.GetWhoFollowPeer(ctx, &friends_proto.GetWhoFollowPeerIn{Uuid: userUUID.String()})
+		assert.Error(t, err, repoErr)
+
+	})
+}
