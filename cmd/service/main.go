275a5ee55017f0043c426bdf02f94c7adb40ce6d
njohanne <aleksey88.avt@gmail.com>
2025-02-27 22:42:22 +0300

    fix_comment

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -23,17 +23,17 @@ func main() {
 	cfg := config.MustLoad()
 	dbRepo, err := db.New(cfg)
 
-	logger := logger_lib.New(cfg.Logger.Host, cfg.Logger.Port, cfg.Service.Name, cfg.Platform.Env)
-
 	if err != nil {
-		log.Printf("postgres.New: %v", err)
+		log.Fatalf("failed to postgres.New: %v", err)
 		os.Exit(1)
 	}
 	defer dbRepo.Close()
 
+	logger := logger_lib.New(cfg.Logger.Host, cfg.Logger.Port, cfg.Service.Name, cfg.Platform.Env)
+
 	metrics, err := pkg.NewMetrics(cfg.Metrics.Host, cfg.Metrics.Port, "friends", cfg.Platform.Env)
 	if err != nil {
-		log.Fatalln("fail to create metrics:", err)
+		log.Fatalln("failed to create metrics:", err)
 	}
 
 	// добавление grpc сервера
@@ -48,19 +48,17 @@ func main() {
 	)
 	friends.RegisterFriendsServiceServer(s, thisService)
 
-	log.Println("start server", cfg.Service.Port)
-
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.Service.Port))
 	if err != nil {
-		log.Printf("Cannot listen port: %s; Error: %s", cfg.Service.Port, err)
+		log.Printf("failed to cannot listen port: %s; Error: %s", cfg.Service.Port, err)
 	}
 
 	_, err = user.New(cfg)
 	if err != nil {
-		log.Printf("Cannot connect User service: %s", err)
+		log.Printf("failed to cannot connect User service: %s", err)
 	}
 
 	if err = s.Serve(lis); err != nil {
-		log.Printf("Cannot start service: %s; Error: %s", cfg.Service.Port, err)
+		log.Printf("failed to cannot start service: %s; Error: %s", cfg.Service.Port, err)
 	}
 }
